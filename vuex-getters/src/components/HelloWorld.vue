71a82d1c5efb9166b9a207d4aae7bec85791ca70
kukaro <justkukaro@naver.com>
2019-06-18 08:50:13 +0000

    add vuex getters

---
+++ vuex-getters/src/components/HelloWorld.vue
@@ -0,0 +1,31 @@
+<!--HelloWorld.vue-->
+<template>
+    <div class="hello">
+        <b>count : {{this.$store.state.count}}</b><br>
+        <b>count^2 : {{this.$store.getters.count}}</b><br>
+        <input type="button" @click="increment()" value="increment"/>
+        <input type="button" @click="decrement()" value="decrement"/>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'HelloWorld',
+        data() {
+            return {}
+        },
+        created: function () {
+        },
+        methods: {
+            increment: function () {
+                this.$store.commit('increment')
+            },
+            decrement: function () {
+                this.$store.commit('decrement')
+            }
+        }
+    }
+</script>
+
+<style scoped>
+</style>
---
+++ vuex-getters/src/store.js
@@ -0,0 +1,28 @@
+/* store.js */
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+    state: {
+        count: 0,
+        weight: 2,
+    },
+    mutations: {
+        increment(state) {
+            state.count++;
+        },
+        decrement(state) {
+            state.count--;
+        }
+    },
+    getters:{
+        count(state, getters){
+            return Math.pow(state.count, getters.weight);
+        },
+        weight(state, /*getters*/){
+            return state.weight;
+        }
+    }
+})
